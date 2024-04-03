395b38837777307a6a6b6115810e2b175eec000e
irfankully123 <150472671+irfankully123@users.noreply.github.com>
2024-04-04 01:03:26 +0500

    Some Necesary Changes

--- lib/screens/ChatTab.dart
+++ lib/screens/ChatTab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'SingleChatWidget.dart';
 
 class ChatsTab extends StatelessWidget {
   const ChatsTab({Key? key}) : super(key: key);
@@ -66,61 +67,3 @@ class ChatsTab extends StatelessWidget {
   }
 }
 
-class SingleChatWidget extends StatelessWidget {
-  final String? chatMessage;
-  final String? chatTitle;
-  final Color? seenStatusColor;
-  final String? imageUrl;
-
-  const SingleChatWidget({
-    Key? key,
-    this.chatMessage,
-    this.chatTitle,
-    this.seenStatusColor,
-    this.imageUrl,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        CircleAvatar(
-          radius: 30,
-          backgroundImage: NetworkImage(imageUrl!),
-        ),
-        Expanded(
-          child: ListTile(
-            title: Text('$chatTitle',
-                style: const TextStyle(fontWeight: FontWeight.w600)),
-            subtitle: Row(children: [
-              Icon(
-                seenStatusColor == Colors.blue ? Icons.done_all : Icons.done,
-                size: 15,
-                color: seenStatusColor,
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 6.0),
-                  child: Text(
-                    '$chatMessage',
-                    style: const TextStyle(overflow: TextOverflow.ellipsis),
-                  ),
-                ),
-              ),
-            ]),
-            trailing: Column(
-              children: const [
-                Padding(
-                  padding: EdgeInsets.only(top: 8.0),
-                  child: Text(
-                    'Yesterday',
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
